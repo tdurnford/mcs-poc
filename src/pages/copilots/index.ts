29c591ebd841fb3d0f0473ce0742b0110731a297
TJ Durnford <thdurnfo@microsoft.com>
2024-02-22 21:00:12 -0700

    Build

--- src/pages/copilots/index.ts
+++ src/pages/copilots/index.ts
@@ -8,7 +8,6 @@ const Copilots = lazy(() => import("./Copilots"));
 const BotIcon = bundleIcon(Bot20Filled, Bot20Regular);
 
 export const registerCopilotPage = createPageRegisterFn({
-  priority: 10,
   title: "Copilot",
   route: "/environments/:environmentId/copilots",
   level: "app",
